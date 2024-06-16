843072b5d981be11a72cd847832793fca3d3be37
Jannik Scheider <js509@hdm-stuttgart.de>
2024-06-16 19:26:54 +0200

    no toggle

--- src/main/java/com/kanezi/render/bootrenderdeploy/BootRenderDeployApplication.java
+++ src/main/java/com/kanezi/render/bootrenderdeploy/BootRenderDeployApplication.java
@@ -2,28 +2,17 @@ package com.kanezi.render.bootrenderdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @SpringBootApplication
 public class BootRenderDeployApplication {
 
-	private static final AtomicBoolean HEALTH_STATUS = new AtomicBoolean(true);
-
 	public static void main(String[] args) {
 		SpringApplication.run(BootRenderDeployApplication.class, args);
-	}
-
-	@Bean
-	public HealthIndicator customHealthIndicator() {
-		return () -> HEALTH_STATUS.get() ? Health.up().build() : Health.down().build();
 	}
 
 	@RestController
@@ -32,13 +21,6 @@ public class BootRenderDeployApplication {
 		@GetMapping
 		String showTimestamp() {
 			return LocalDateTime.now().toString();
-		}
-
-		@GetMapping("/toggleHealth")
-		String toggleHealth() {
-			boolean currentStatus = HEALTH_STATUS.get();
-			HEALTH_STATUS.set(!currentStatus);
-			return "Health status set to " + (currentStatus ? "DOWN" : "UP");
 		}
 	}
 }
